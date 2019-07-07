860f2cb99cc45eaf03c5df3023153753dde23992
Esbjörn Redmo <esbjorn.redmo@gmail.com>
2019-07-07 15:01:51 +0100

    Remove -rc tag

--- source/conf.py
+++ source/conf.py
@@ -26,7 +26,7 @@ author = u'Esbjörn Redmo'
 # The short X.Y version
 version = '1.0.0'
 # The full version, including alpha/beta/rc tags
-release = '1.0.0-rc'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
